315cac6390b6ce7262fbc44ae109d3206fce3149
smetan411 <anna.smetanova@atlas.cz>
2023-04-13 13:37:05 +0200

    stavby

--- src/main/java/domky/vesnicka/Vesnicka.java
+++ src/main/java/domky/vesnicka/Vesnicka.java
@@ -2,6 +2,7 @@ package domky.vesnicka;
 
 import domky.pocatek.AbsLocation;
 import domky.stavby.Domek;
+import domky.stavby.DomekVetsiOkna;
 import org.bukkit.World;
 
 public class Vesnicka {
@@ -21,35 +22,32 @@ public class Vesnicka {
         this.vyskaSten = vyskaSten;
         this.pocetPater = pocetPater;
     }
+
     public void postavVesnicku() {
 
         int[][] mapa = {
-                {1, 0, 1, 0},
-                {0, 1, 0, 1},
-                {1, 0, 1, 0},
-                {1, 0, 0, 1},
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0},
+                {2, 0, 2, 0, 2},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1}
         };
 
         Domek domek = new Domek();
+        DomekVetsiOkna domek1 = new DomekVetsiOkna();
         AbsLocation pocatekDomu = pocatekVesnice;
         for (int sloupec = 0; sloupec < mapa.length; sloupec++) {
             for (int radek = 0; radek < mapa[sloupec].length; radek++) {
                 if (mapa[radek][sloupec] == 1) {
                     domek.postavDomek(svet, pocatekDomu, 10, 10, 6);
                 }
+                if (mapa[radek][sloupec] == 2) {
+                    domek1.postavDomek(svet, pocatekDomu, 10, 10, 6);
+                }
                 pocatekDomu = pocatekDomu.plus(sirkaDomu, 0, 0);
-
             }
-            pocatekDomu = pocatekDomu.plus(-sirkaDomu*mapa[0].length, 0, delkaDomu);
-
+            pocatekDomu = pocatekDomu.plus(-sirkaDomu * mapa[0].length, 0, delkaDomu);
         }
     }
-    //        for (int[]sloupec :mapa ) {
-//            for (int radek: sloupec) {
-//                if (radek == 1) {
-//                    domek.postavDomek(svet, pocatekDomu, 10, 10, 6);
-//                }
-//            }
-//        }
 }
 
